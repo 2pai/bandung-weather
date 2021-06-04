372c066274e935938f616840bfbacb02aec75526
alifnaufalyasin <alifnaufal171199@gmail.com>
2021-06-04 18:48:47 +0700

    update bisa publish random sensor

--- seeder.js
+++ seeder.js
@@ -1,19 +1,22 @@
 const mqtt = require('mqtt')
-const client  = mqtt.connect('mqtt://localhost')
+const client = mqtt.connect('mqtt://172.17.85.17', {clientId: "Sensor-Data"})
 const { v4: uuidv4 } = require('uuid');
 const seed = (topic, value) => {
   client.publish(topic, value)
 }
 
 const genValue = () => {
-  const min = 10;
-  const max = 60;
+  const min = 1;
+  const max = 70;
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 setInterval(() => {
   const uuid = uuidv4();
-  seed('sensor-1', JSON.stringify({id:uuid, deviceName:'sensor-1',date:Date.now(), value: genValue()}))
-  seed('sensor-2', JSON.stringify({id:uuid, deviceName:'sensor-2',date:Date.now(), value: genValue()}))
-  seed('sensor-3', JSON.stringify({id:uuid, deviceName:'sensor-3',date:Date.now(), value: genValue()}))
+  const randNum = (Math.floor(Math.random() * 5)+1)
+  console.log(randNum);
+  for (let i = 1; i <= randNum; i++) {
+    console.log(`sensor-${i}`)
+    seed(`sensor-${i}`, JSON.stringify({id:uuid, deviceName:`sensor-${i}`,date:Date.now(), value: genValue(), manySensor:randNum}))
+  }
 
-}, 10000);
+}, 5000);
